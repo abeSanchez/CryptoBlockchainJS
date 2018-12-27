0730e1f8302a19d1c08e4c58764b66db71582518
abeSanchez <abraham5001@aol.com>
2018-12-27 02:54:41 -0500

    Backend ready

--- blockchain/index.js
+++ blockchain/index.js
@@ -1,5 +1,8 @@
 const Block = require('./block');
+const Transaction = require('../wallet/transaction');
+const Wallet = require('../wallet');
 const { cryptoHash } = require('../util'); 
+const { REWARD_INPUT, MINING_REWARD } = require('../config');
 
 class Blockchain {
     constructor() {
@@ -13,6 +16,54 @@ class Blockchain {
         });
 
         this.chain.push(newBlock);
+    }
+
+    validTransactionData({ chain }) {
+        for (let i = 1; i < chain.length; i++) {
+            const block = chain[i];
+            const transactionSet = new Set();
+            let rewardTransactionCount = 0;
+
+            for (let transaction of block.data) {
+                if (transaction.input.address === REWARD_INPUT.address) {
+                    rewardTransactionCount++;
+
+                    if (rewardTransactionCount > 1) {
+                        console.error('Miner rewards exceed limit');
+                        return false;
+                    }
+                        
+                    if (Object.values(transaction.outputMap)[0] !== MINING_REWARD) {
+                        console.error('Miner reward amount is invalid');
+                    }
+                } else {
+                    if (!Transaction.validTransaction(transaction)) {
+                        console.error('Invalid transaction');
+                        return false;
+                    }
+
+                    const trueBalance = Wallet.calculateBalance({
+                        chain: this.chain,
+                        address: transaction.input.address
+                    });
+
+                    if (transaction.input.amount !== trueBalance) {
+                        console.error('Invalid input amount');
+                        return false;
+                    }
+
+                    if (transactionSet.has(transaction)) {
+                        console.error('An identical transaction appears more than once in the block');
+                        return false;
+                    } else {
+                        transactionSet.add(transaction);
+                    }
+                }
+            }
+
+        }
+        
+        return true;
     }
 
     static isValidChain(chain) {
@@ -32,14 +83,17 @@ class Blockchain {
         return true;
     }
 
-    replaceChain(chain) {
+    replaceChain(chain, validateTransactions, onSuccess) {
         if (chain 
             && chain.length > this.chain.length 
-            && Blockchain.isValidChain(chain))
-            this.chain = chain;
+            && Blockchain.isValidChain(chain)
+            && (validateTransactions && this.validTransactionData({ chain }) || !validateTransactions)) {
+                if (onSuccess) onSuccess();
+                this.chain = chain;
+            }
         
         if (chain && chain.length <= this.chain.length)
-            console.log('Incoming chain must be longer to replace');
+            console.error('Incoming chain must be longer to replace');
             
     }
 }
--- index.js
+++ index.js
@@ -3,15 +3,16 @@ const express = require('express');
 const request = require('request');
 const Blockchain = require('./blockchain');
 const PubSub = require('./app/pubsub');
-const TransactionPool = require('./wallet/transoaction-pool');
-const Wallet = require('./wallet')
+const TransactionPool = require('./wallet/transaction-pool');
+const Wallet = require('./wallet');
+const TransactionMiner = require('./app/transaction-miner');
 
 const app = express();
 const blockchain = new Blockchain();
 const transactionPool = new TransactionPool();
 const wallet = new Wallet();
 const pubsub = new PubSub({ blockchain, transactionPool });
-
+const transactionMiner = new TransactionMiner({ blockchain, transactionPool, wallet, pubsub });
 
 const DEFAULT_PORT = 3000;
 const ROOT_NODE_ADDRESS = `http://localhost:${DEFAULT_PORT}`;
@@ -43,7 +44,11 @@ app.post('/api/transact', (req, res) => {
         if (transaction) {
             transaction.update({ senderWallet: wallet, recipient, amount });
         } else {
-            transaction = wallet.createTransaction({ recipient, amount });
+            transaction = wallet.createTransaction({ 
+                recipient, 
+                amount, 
+                chain: blockchain.chain 
+            });
         }
     } catch (error) {
         return res.status(400).json({ type: 'error', message: error.message });
@@ -58,6 +63,20 @@ app.post('/api/transact', (req, res) => {
 
 app.get('/api/transaction-pool-map', (req, res) => {
     res.json(transactionPool.transactionMap);
+});
+
+app.get('/api/mine-transactions', (req, res) => {
+    transactionMiner.mineTransactions();
+
+    res.redirect('/api/blocks');
+});
+
+app.get('/api/wallet-info', (req, res) => {
+    const address = wallet.publicKey;
+    res.json({
+        address,
+        balance: Wallet.calculateBalance({ chain: blockchain.chain, address })
+    });
 });
 
 const syncWithRootState = () => {
